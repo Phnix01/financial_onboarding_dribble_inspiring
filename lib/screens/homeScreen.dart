062ae81068d4fb061e5e97b05e4c64201fe241cd
Phnix01 <fomar9235@gmail.com>
2024-10-20 02:04:51 +0100

    insertion du positionned circle rose

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -7,70 +7,90 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 500,
-                decoration:
-                    BoxDecoration(color: const Color.fromARGB(8, 0, 0, 0)),
-              ),
-              Center(
-                child: Image.asset(
-                  'assets/portrait.png',
-                  height: 500,
-                  fit: BoxFit.fitWidth,
+      body: Stack(children: [
+        Positioned(
+            left: -100,
+            bottom: 360,
+            child: Container(
+              width: 200,
+              height: 150,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color.fromARGB(115, 250, 96, 211)),
+            )),
+        Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 450,
+                  decoration:
+                      BoxDecoration(color: const Color.fromARGB(8, 0, 0, 0)),
                 ),
-              ),
-              Positioned(
-                top: 40,
-                right: 40,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Skip',
-                    style: TextStyle(
-                      color: Colors.black,
+                Center(
+                  child: Image.asset(
+                    'assets/portrait.png',
+                    height: 450,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  right: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 40,
-              horizontal: 35,
-            ),
-            child: Column(
-              children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  'Unleash Your \n Financial Potential',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  textAlign: TextAlign.center,
-                  'Artificial intelligence for smarter \n financial decisions',
-                  style: TextStyle(fontSize: 19, color: Colors.black87),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                CustomButton(textDta: 'Login'),
               ],
             ),
-          ),
-        ],
-      ),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 40,
+                horizontal: 35,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Unleash Your \n Financial Potential',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Artificial intelligence for smarter \n financial decisions',
+                    style: TextStyle(fontSize: 19, color: Colors.black87),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  CustomButton(textDta: 'Login'),
+                ],
+              ),
+            ),
+            Positioned(
+                top: 22,
+                left: 50,
+                // bottom: 20,
+                child: Container(
+                  width: 80,
+                  color: Colors.green,
+                )),
+          ],
+        ),
+      ]),
     );
   }
 }
