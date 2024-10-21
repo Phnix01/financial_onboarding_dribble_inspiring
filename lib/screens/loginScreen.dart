917444f5f08d022ac9270cf77a42217ec83a144c
Phnix01 <fomar9235@gmail.com>
2024-10-21 08:21:01 +0100

    structure de la login Screen terminée, sans utilisé la widget stack

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,3 +1,4 @@
+import 'package:financial_onboarding_dribble_inspiring/Components/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,113 +9,113 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Stack(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Positioned(
-                top: 40,
-                right: 40,
+              Align(
+                alignment: Alignment.topRight,
                 child: ElevatedButton(
-                  onPressed: () {
-                    // Action pour Skip
-                  },
-                  child: const Text(
+                  onPressed: () {},
+                  child: Text(
                     'Skip',
-                    style: TextStyle(color: Colors.black),
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-              Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              const SizedBox(
+                height: 50,
+              ),
+              const SizedBox(height: 40),
+              // Welcome text with emoji
+              const Text(
+                'Hey 👋',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Login Now!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
                 children: [
                   Text(
-                    'Hey 👋',
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'Login Now!',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'I Am A Old User /',
-                        style: TextStyle(
-                          fontSize: 17,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          'Create New',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
+                    'I\'m An Old User /',
+                    style: TextStyle(color: Colors.grey.shade500, fontSize: 18),
                   ),
                   const SizedBox(
-                    height: 40,
+                    width: 10,
                   ),
-                  Container(
-                    child: TextField(
-                      decoration: InputDecoration(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Create New',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    child: TextField(
-                      decoration: InputDecoration(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Forgot Password? /',
-                        style: TextStyle(
-                          fontSize: 17,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          'Reset',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                  )
                 ],
-              )),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                decoration: InputDecoration(),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                decoration: InputDecoration(),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Forgot Password? ',
+                    style: TextStyle(color: Colors.grey.shade500, fontSize: 18),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Reset',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              CustomButton(
+                textDta: 'Login now',
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text('Crée par Omar Farouk'),
+              )
             ],
           ),
         ),
